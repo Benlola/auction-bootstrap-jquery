f7d1d7f053c05f10fc444d0dc196dd499ace1ce1
foxapp <enache.ion.vas@gmail.com>
2022-06-16 23:43:49 +0300

    Blocke run settings collection into command line

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ use App\Models\SupportTicket;
 use App\Models\User;
 use App\Models\Withdrawal;
 use App\Models\Product;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 
@@ -37,56 +38,56 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
-        $activeTemplate = activeTemplate();
-        $general = GeneralSetting::first();
-        $viewShare['general'] = $general;
-        $viewShare['activeTemplate'] = $activeTemplate;
-        $viewShare['activeTemplateTrue'] = activeTemplate(true);
-        $viewShare['language'] = Language::all();
-        $viewShare['pages'] = Page::where('tempname',$activeTemplate)->where('is_default',0)->get();
+        if(!App::runningInConsole()){
+            $activeTemplate = activeTemplate();
+            $general = GeneralSetting::first();
+            $viewShare['general'] = $general;
+            $viewShare['activeTemplate'] = $activeTemplate;
+            $viewShare['activeTemplateTrue'] = activeTemplate(true);
+            $viewShare['language'] = Language::all();
+            $viewShare['pages'] = Page::where('tempname',$activeTemplate)->where('is_default',0)->get();
 
-        view()->share($viewShare);
-        
+            view()->share($viewShare);
 
-        view()->composer('admin.partials.sidenav', function ($view) {
-            $view->with([
-                'banned_users_count'           => User::banned()->count(),
-                'email_unverified_users_count' => User::emailUnverified()->count(),
-                'sms_unverified_users_count'   => User::smsUnverified()->count(),
-                'banned_merchants_count'           => Merchant::banned()->count(),
-                'email_unverified_merchants_count' => Merchant::emailUnverified()->count(),
-                'sms_unverified_merchants_count'   => Merchant::smsUnverified()->count(),
-                'pending_user_ticket_count'         => SupportTicket::where('user_id', '!=', 0)->whereIN('status', [0,2])->count(),
-                'pending_merchant_ticket_count'         => SupportTicket::where('merchant_id', '!=', 0)->whereIN('status', [0,2])->count(),
-                'pending_deposits_count'    => Deposit::pending()->count(),
-                'pending_withdraw_count'    => Withdrawal::pending()->count(),
-                'live_product_count' => Product::live()->count(),
-                'pending_product_count' => Product::pending()->count(),
-                'upcoming_product_count' => Product::upcoming()->count(),
-                'expired_product_count' => Product::expired()->count(),
-            ]);
-        });
 
-        view()->composer('admin.partials.topnav', function ($view) {
-            $view->with([
-                'adminNotifications'=>AdminNotification::where('read_status',0)->with('user')->orderBy('id','desc')->get(),
-            ]);
-        });
+            view()->composer('admin.partials.sidenav', function ($view) {
+                $view->with([
+                    'banned_users_count'           => User::banned()->count(),
+                    'email_unverified_users_count' => User::emailUnverified()->count(),
+                    'sms_unverified_users_count'   => User::smsUnverified()->count(),
+                    'banned_merchants_count'           => Merchant::banned()->count(),
+                    'email_unverified_merchants_count' => Merchant::emailUnverified()->count(),
+                    'sms_unverified_merchants_count'   => Merchant::smsUnverified()->count(),
+                    'pending_user_ticket_count'         => SupportTicket::where('user_id', '!=', 0)->whereIN('status', [0,2])->count(),
+                    'pending_merchant_ticket_count'         => SupportTicket::where('merchant_id', '!=', 0)->whereIN('status', [0,2])->count(),
+                    'pending_deposits_count'    => Deposit::pending()->count(),
+                    'pending_withdraw_count'    => Withdrawal::pending()->count(),
+                    'live_product_count' => Product::live()->count(),
+                    'pending_product_count' => Product::pending()->count(),
+                    'upcoming_product_count' => Product::upcoming()->count(),
+                    'expired_product_count' => Product::expired()->count(),
+                ]);
+            });
 
-        view()->composer('partials.seo', function ($view) {
-            $seo = Frontend::where('data_keys', 'seo.data')->first();
-            $view->with([
-                'seo' => $seo ? $seo->data_values : $seo,
-            ]);
-        });
+            view()->composer('admin.partials.topnav', function ($view) {
+                $view->with([
+                    'adminNotifications'=>AdminNotification::where('read_status',0)->with('user')->orderBy('id','desc')->get(),
+                ]);
+            });
 
-        if($general->force_ssl){
-            \URL::forceScheme('https');
+            view()->composer('partials.seo', function ($view) {
+                $seo = Frontend::where('data_keys', 'seo.data')->first();
+                $view->with([
+                    'seo' => $seo ? $seo->data_values : $seo,
+                ]);
+            });
+
+            if($general->force_ssl){
+                \URL::forceScheme('https');
+            }
+
+
+            Paginator::useBootstrap();
         }
-
-
-        Paginator::useBootstrap();
-        
     }
 }
