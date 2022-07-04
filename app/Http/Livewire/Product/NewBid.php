b955d0766c486597ca5fc880346728fe7d13b018
foxapp <enache.ion.vas@gmail.com>
2022-07-04 22:47:21 +0300

    Removed some small mistakes

--- app/Http/Livewire/Product/NewBid.php
+++ app/Http/Livewire/Product/NewBid.php
@@ -64,7 +64,7 @@ class NewBid extends Component
             //dd($this->product->price, $this->amount);
 
             if ((int)$this->product->price > (int)$this->amount) {
-                $this->addError('amount', 'Bid amount must be greater than product price 65');
+                $this->addError('amount', 'Bid amount must be greater than product price');
 
                 return back();
             }
@@ -106,7 +106,7 @@ class NewBid extends Component
             $user = auth()->user();
 
             if ($this->product->price > $this->amount) {
-                $this->addError('amount', 'Bid amount must be greater than product price 105');
+                $this->addError('amount', 'Bid amount must be greater than product price');
 
                 return back();
             }
